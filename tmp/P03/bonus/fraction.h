43c797b6c3e92a3cfc9c1e13110c7d866a9204d5
JamesdeLeon <jdeleonphys@gmail.com>
2022-06-10 18:44:38 -0500

    basic outline formed; add functions, documentation, coherence

---
+++ tmp/P03/bonus/fraction.h
@@ -0,0 +1,37 @@
+#ifndef __FRACTION_H
+#define __FRACTION_H
+
+#include <iostream>
+
+class Fraction
+{
+	public:
+		Fraction(int num = 0, int den = 1);
+
+		Fraction operator-();
+		Fraction operator+(const Fraction& rhs);
+		Fraction operator-(const Fraction& rhs);
+		Fraction operator*(const Fraction& rhs);
+		Fraction operator/(const Fraction& rhs);
+
+		inline bool operator==(const Fraction& rhs) {return compare(rhs) == 0;}
+		inline bool operator!=(const Fraction& rhs) {return compare(rhs) != 0;}
+		inline bool operator<(const Fraction& rhs) {return compare(rhs) == -1;}
+		inline bool operator<=(const Fraction& rhs) {return compare(rhs) <= 0;}
+		inline bool operator>(const Fraction& rhs) {return compare(rhs) == 1;}
+		inline bool operator>=(const Fraction& rhs) {return compare(rhs) >= 0;}
+
+		friend std::ostream& operator<<(std::ostream& ost, Fraction& m);
+		friend std::istream& operator>>(std::istream& ist, Fraction& m);
+		friend bool operator!=(const Fraction& lhs, const Fraction& rhs);
+	private:
+		int _num;
+		int _den;
+
+		void reduce();
+		int compare(const Fraction& rhs);
+};
+
+int gcd(int a, int b);
+
+#endif
---
+++ tmp/P03/bonus/main.cpp
@@ -0,0 +1,30 @@
+#include "fraction.h"
+
+int main(void)
+{
+	Fraction frac1, frac2;
+
+	std::cout << "Enter two fractions: <n/d <Enter> n/d>" << std::endl;
+	std::cin >> frac1;
+	std::cin >> frac2;
+
+	std::cout << "Fraction #1: " << frac1 << std::endl;
+	std::cout << "Fraction #2: " << frac2 << std::endl;
+
+	Fraction frac1_neg = -frac1;
+	Fraction frac2_neg = -frac2;
+	std::cout << "Negation of Fraction #1: " << frac1_neg << std::endl;
+	std::cout << "Negation of Fraction #2: " << frac2_neg << std::endl;
+
+	Fraction frac_add = frac1 + frac2;
+	std::cout << "Addition: " << frac_add << std::endl;
+
+	Fraction frac_subtract = frac1 - frac2;
+	std::cout << "Subtraction: " << frac_subtract << std::endl;
+
+	Fraction frac_mult = frac1 * frac2;
+	std::cout << "Multiplication: " << frac_mult << std::endl;
+
+	Fraction frac_div = frac1 / frac2;
+	std::cout << "Division : " << frac_div << std::endl;
+}
